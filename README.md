1ef204a442b7b1cd46f55bb2c1db1e02b2324874
BSPANGLER <bspangler21@alumni.uwosh.edu>
2023-12-29 15:48:17 -0600

    Update subproject commits and add run-locally script

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # GolfLeagueTracker
+
+
+cd D:\repos\GolfLeagueTracker
+powershell -executionpolicy bypass -File .\run-locally.ps1
---
+++ run-locally.ps1
@@ -0,0 +1,4 @@
+start-process powershell -WorkingDirectory "Golf-Tracker" -ArgumentList '-noexit -command npm run dev'
+start-process powershell -WorkingDirectory "GolfTrackerAPI/GolfTrackerAPI" -ArgumentList '-noexit -command dotnet run'
+cd Golf-Tracker
+code-insiders .
